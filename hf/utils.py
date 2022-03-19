9dc271255c867fb70cb3eb4384f59e44ef6e77a6
Nicholas Broad <nicholas@nmbroad.com>
2022-03-19 12:22:26 -0400

    set wandb env vars, sigmoid, get_location_predictions
    reinit_model_weights
    layerwise_learning_rate
    uniform_learning_rate

--- hf/utils.py
+++ hf/utils.py
@@ -1,3 +1,4 @@
+import os
 from itertools import chain
 from dataclasses import dataclass
 
@@ -5,6 +6,53 @@ import torch
 import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 from transformers import DataCollatorForTokenClassification
+from transformers.utils import logging
+
+logger = logging.get_logger(__name__)
+
+
+def set_wandb_env_vars(cfg):
+    os.environ["WANDB_ENTITY"] = cfg["entity"]
+    os.environ["WANDB_PROJECT"] = cfg["project"]
+    os.environ["WANDB_RUN_GROUP"] = cfg["group"]
+    os.environ["WANDB_JOB_TYPE"] = cfg["job_type"]
+    os.environ["WANDB_NOTES"] = cfg["notes"]
+    os.environ["WANDB_TAGS"] = cfg["tags"]
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def get_location_predictions(preds, dataset):
+    """
+    Finds the prediction indexes at the character level.
+    """
+    all_predictions = []
+    for pred, offsets, seq_ids, text in zip(
+        preds, dataset["offset_mapping"], dataset["sequence_ids"], dataset["pn_history"]
+    ):
+        pred = sigmoid(pred)
+        start_idx = None
+        current_preds = []
+        for p, o, s_id in zip(pred, offsets, seq_ids):
+            if s_id is None or s_id == 0:
+                continue
+
+            if p > 0.5:
+                if start_idx is None:
+                    start_idx = o[0]
+                end_idx = o[1]
+            elif start_idx is not None:
+                current_preds.append((start_idx, end_idx))
+                start_idx = None
+
+        if start_idx is not None:
+            current_preds.append((start_idx, end_idx))
+
+        all_predictions.append(current_preds)
+
+    return all_predictions
 
 
 def kaggle_metrics(eval_prediction, dataset):
@@ -14,45 +62,52 @@ def kaggle_metrics(eval_prediction, dataset):
     Use partial for the args and kwargs to pass other data
     into the `compute_metrics` function.
     """
+
+    preds = get_location_predictions(eval_prediction.predictions, dataset)
+
     all_labels = []
     all_preds = []
     for preds, offsets, seq_ids, labels, text in zip(
-        eval_prediction.predictions, 
-        dataset["offset_mapping"], 
-        dataset["sequence_ids"], 
-        dataset["labels"], 
-        dataset["text"]
+        eval_prediction.predictions,
+        dataset["offset_mapping"],
+        dataset["sequence_ids"],
+        dataset["labels"],
+        dataset["text"],
     ):
-        
+
         num_chars = max(list(chain(*offsets)))
         char_labels = np.zeros((num_chars), dtype=bool)
-        
+
         for (tok_start, tok_end), s_id, label in zip(offsets, seq_ids, labels):
-            if s_id is None or s_id == 0: # ignore question part of input
+            if s_id is None or s_id == 0:  # ignore question part of input
                 continue
             if int(label) == 1:
-                
+
                 char_labels[tok_start:tok_end] = 1
-                if text[tok_start].isspace() and tok_start>0 and not char_labels[tok_start-1]:
+                if (
+                    text[tok_start].isspace()
+                    and tok_start > 0
+                    and not char_labels[tok_start - 1]
+                ):
                     char_labels[tok_start] = 0
-                    
+
         char_preds = np.zeros((num_chars))
-        
+
         for start_idx, end_idx in preds:
             char_preds[start_idx:end_idx] = 1
-            if text[start_idx].isspace() and start_idx>0 and not char_preds[start_idx-1]:
+            if (
+                text[start_idx].isspace()
+                and start_idx > 0
+                and not char_preds[start_idx - 1]
+            ):
                 char_preds[start_idx] = 0
-            
+
         all_labels.extend(char_labels)
         all_preds.extend(char_preds)
-        
+
     results = precision_recall_fscore_support(all_labels, all_preds, average="binary")
-    
-    return {
-            "precision": results[0],
-            "recall": results[1],
-            "f1": results[2]
-        } 
+
+    return {"precision": results[0], "recall": results[1], "f1": results[2]}
 
 
 @dataclass
@@ -102,3 +157,101 @@ class DataCollatorWithMasking(DataCollatorForTokenClassification):
         )
 
         return batch
+
+
+def reinit_model_weights(model, n_layers, config):
+
+    if config.model_type == "bart":
+        encoder_layers = model.encoder.layers
+        decoder_layers = model.decoder.layers
+
+        reinit_layers(encoder_layers, n_layers, config)
+        reinit_layers(decoder_layers, n_layers, config)
+    else:
+        encoder_layers = model.encoder.layer
+        reinit_layers(encoder_layers, n_layers, config)
+ 
+ 
+def reinit_layers(layers, n_layers, config):
+
+    if config.model_type == "bart":
+        std = config.init_std    
+    else:
+        std = config.initializer_range
+
+    logger.info(f"Reinitializing last {n_layers} layers in the encoder.")
+    for layer in layers[-n_layers:]:
+        for module in layer.modules():
+            if isinstance(module, torch.nn.Linear):
+                module.weight.data.normal_(mean=0.0, std=std)
+                if module.bias is not None:
+                    module.bias.data.zero_()
+            elif isinstance(module, torch.nn.Embedding):
+                module.weight.data.normal_(mean=0.0, std=std)
+                if module.padding_idx is not None:
+                    module.weight.data[module.padding_idx].zero_()
+            elif isinstance(module, torch.nn.LayerNorm):
+                module.bias.data.zero_()
+                module.weight.data.fill_(1.0)
+
+
+def layerwise_learning_rate(model, lr=3e-5, wd=0.01, alpha=0.8):
+    model_type = model.backbone_name
+
+    layers = (
+        [getattr(model, model_type).embeddings]
+        + [getattr(model, model_type).encoder.layer]
+        + [model.output]
+    )
+    layers.reverse()
+
+    optimizer_grouped_parameters = []
+
+    no_decay = ["bias", "LayerNorm.weight"]
+    for layer in layers:
+        lr *= alpha
+        optimizer_grouped_parameters += [
+            {
+                "params": [
+                    p
+                    for n, p in layer.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": wd,
+                "lr": lr,
+            },
+            {
+                "params": [
+                    p
+                    for n, p in layer.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.0,
+                "lr": lr,
+            },
+        ]
+
+    return optimizer_grouped_parameters
+
+
+def uniform_learning_rate(model, wd=0.01):
+
+    no_decay = ["bias", "LayerNorm.weight"]
+    return [
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": wd,
+        },
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
