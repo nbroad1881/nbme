dd4e035cbaf43265f240268477164e2509f05de4
Nicholas Broad <nicholas@nmbroad.com>
2022-03-20 13:40:25 -0400

    wandb.finish
    more callbacks
    change config each fold

--- hf/run.py
+++ hf/run.py
@@ -1,7 +1,8 @@
 from functools import partial
 import datetime
 
-from transformers import Trainer, AutoConfig
+import wandb
+from transformers import Trainer, TrainingArguments, AutoConfig
 from transformers.trainer_utils import set_seed
 from transformers.integrations import WandbCallback
 
@@ -15,11 +16,12 @@ from utils import (
     set_wandb_env_vars,
     reinit_model_weights,
 )
-from callbacks import NewWandbCB, SaveCallback
+from callbacks import NewWandbCB, SaveCallback, MaskingProbCallback
 
 if __name__ == "__main__":
 
     config_file = "q-rob1.yml"
+    output = "rob-l"
     cfg, args = get_configs(config_file)
     set_seed(cfg["seed"])
     set_wandb_env_vars(cfg)
@@ -28,12 +30,22 @@ if __name__ == "__main__":
 
     for fold in range(cfg["k_folds"]):
 
+
+        cfg, args = get_configs(config_file)
         cfg["fold"] = fold
+        args["output_dir"] = f"{output}_f{fold}"
+
+        args = TrainingArguments(**args)
 
         with args.main_process_first(desc="dataset pre-processing"):
-            datamodule.prepare_datsets(fold=fold)
+            datamodule.prepare_datasets(fold=fold)
 
-            wb_callback = NewWandbCB(cfg)
+        # Callbacks
+        wb_callback = NewWandbCB(cfg)
+        save_callback = SaveCallback(
+            min_score_to_save=cfg["min_score_to_save"], metric_name="eval_f1"
+        )
+        masking_callback = MaskingProbCallback(cfg["masking_prob"])
 
         train_dataset = datamodule.get_train_dataset()
         eval_dataset = datamodule.get_eval_dataset()
@@ -41,12 +53,11 @@ if __name__ == "__main__":
         compute_metrics = partial(kaggle_metrics, dataset=eval_dataset)
 
         model_config = AutoConfig.from_pretrained(cfg["model_name_or_path"])
-
         model_config.update(
             {
                 "num_labels": 1,
                 "hidden_dropout_prob": cfg["dropout"],
-                "layer_norm_eps": cfg["layer_norm_eps"],
+                # "layer_norm_eps": cfg["layer_norm_eps"],
                 "run_start": str(datetime.datetime.utcnow()),
             }
         )
@@ -62,10 +73,9 @@ if __name__ == "__main__":
         data_collator = DataCollatorWithMasking(
             tokenizer=datamodule.tokenizer,
             return_tensors="pt",
+            padding="longest",
             pad_to_multiple_of=8,
-            max_length=cfg["max_seq_length"],
             label_pad_token_id=-100,
-            masking_prob=cfg["masking_prob"],
         )
 
         trainer = Trainer(
@@ -76,10 +86,10 @@ if __name__ == "__main__":
             compute_metrics=compute_metrics,
             tokenizer=datamodule.tokenizer,
             data_collator=data_collator,
-            callbacks=[SaveCallback(min_score_to_save=0.8, metric_name="eval/f1")],
+            callbacks=[wb_callback, save_callback, masking_callback],
         )
 
         trainer.remove_callback(WandbCallback)
-        trainer.add_callback(wb_callback)
 
         trainer.train()
+        wandb.finish()
