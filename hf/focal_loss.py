d834e1a20fe6f725d0f7511303d3dba0a614fb0d
Nicholas Broad <nicholas@nmbroad.com>
2022-03-23 17:43:37 -0400

    remove weird stuff

--- hf/focal_loss.py
+++ hf/focal_loss.py
@@ -2,21 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class FocalLoss(nn.Module):
-    
-    def __init__(self, weight=None, 
-                 gamma=2.0, reduction='none'):
+    def __init__(self, alpha=1, gamma=2.0, reduction="none"):
         nn.Module.__init__(self)
-        self.weight = weight
+        self.alpha = alpha
         self.gamma = gamma
         self.reduction = reduction
-        
-    def forward(self, input_tensor, target_tensor):
-        log_prob = F.log_softmax(input_tensor, dim=-1)
-        prob = torch.exp(log_prob)
-        return F.nll_loss(
-            ((1 - prob) ** self.gamma) * log_prob, 
-            target_tensor, 
-            weight=self.weight,
-            reduction = self.reduction
+
+    def forward(self, inputs, targets):
+        ce_loss = F.binary_cross_entropy(
+            inputs, targets, reduction=self.reduction
         )
+        pt = inputs * targets + (1 - inputs) * (1 - targets)
+
+        loss = ((1 - pt) ** self.gamma) * ce_loss
+        if self.alpha >= 0:
+            alpha_t = self.alpha * targets + (1 - self.alpha) * (1 - targets)
+            loss = alpha_t * loss
+        
+        if self.reduction == "mean":
+            loss = loss.mean()
+        elif self.reduction == "sum":
+            loss = loss.sum()
+
+        return loss 
