6314a2d0aa8f270173923ca36cd41baafc5cef76
Nicholas Broad <nicholas@nmbroad.com>
2022-04-22 11:14:14 -0400

    delete fast debv2toke

--- hf/colab_setup.sh
+++ hf/colab_setup.sh
@@ -24,11 +24,6 @@ mkdir data
 kaggle competitions download -c nbme-score-clinical-patient-notes -p data
 unzip data/nbme-score-clinical-patient-notes.zip -d data
 
-TRANSFORMERS_PATH="$(python -c 'import transformers; from pathlib import Path; print(Path(transformers.__file__).parent)')"
-cp convert_slow_tokenizer.py $TRANSFORMERS_PATH/
-cp tokenization_deberta_v2_fast.py $TRANSFORMERS_PATH/models/deberta_v2/
-cp tokenization_deberta_v2.py $TRANSFORMERS_PATH/models/deberta_v2/
-
 read -s -p "Upload wandb.txt: " ignore
 export WANDB_API_KEY=$( cat wandb.txt )
 wandb login
